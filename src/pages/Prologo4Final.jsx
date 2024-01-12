2e16bf1ee764bf701d1f4237c0760a2ab80f8c51
Marcos <127824847+CMarcosVi@users.noreply.github.com>
2024-01-12 13:54:42 -0300

    Update Prologo4Final.jsx

--- src/pages/Prologo4Final.jsx
+++ src/pages/Prologo4Final.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, hasChaveCarro } from 'react-redux';
 import { addItem } from '../store/inventarioSlice';
 import Personagem from '../personagem/personagem';
 
